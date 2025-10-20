bba30186744d2dac38a3fc23604489d7752eb1c4
muhammad saad <engineer.muhammadsaad688@gmail.com>
2025-10-20 11:00:53 +0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='navbar'>
+<h2>Our Tours</h2>
+<div className='underline'>
+
+</div>
+    </div>
+
+    </>
+  )
+}
+
+export default Navbar
